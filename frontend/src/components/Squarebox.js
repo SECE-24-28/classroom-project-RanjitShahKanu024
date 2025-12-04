0f83b995a70a9df3663cc4f85a2e821d4adb182d
Ranjit Shah Kanu <ranjitshahkanu.2024cse@sece.ac.in>
2025-12-04 10:11:14 +0530

    project push

---
+++ frontend/src/components/Squarebox.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Squarebox.css";
+function Squarebox() {
+  return (
+    <div className="box-component">
+      <div className="box-message">
+        <h1>student</h1>
+      </div>
+      <div className="box-second">
+        <h1>enrolled</h1>
+      </div>
+      <div className="box-third">
+        <h1>client</h1>
+      </div>
+      <div className="box-fourth">
+        <h1>name</h1>
+      </div>
+    </div>
+  );
+}
+export default Squarebox;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Squarebox from "../components/Squarebox";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Squarebox />
     </div>
   );
 };
